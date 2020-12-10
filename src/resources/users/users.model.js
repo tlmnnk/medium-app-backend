535828f06b6e802f057cad6226c66b25c297e647
Vladislav <vt118@mail.ru>
2020-12-10 23:03:54 +0300

    feat: add user model, add mongoose

--- src/resources/users/users.model.js
+++ src/resources/users/users.model.js
@@ -1,2 +1,19 @@
-// TODO:
-// start model
+const mongoose = require('mongoose')
+const uuid = require('uuid')
+
+const userSchema = new mongoose.Schema({
+  bio: String,
+  image: String,
+  username: String,
+  password: String,
+  _id: {
+    type: String,
+    default: uuid,
+  },
+})
+
+const userModel = mongoose.model('User', userSchema)
+
+module.exports = {
+  userModel,
+}
