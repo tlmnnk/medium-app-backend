9c660021d094343fa8b258b17428638f0dee6928
Vladislav <vt118@mail.ru>
2020-12-12 16:59:22 +0300

    feat: add checkAuth middleware

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,24 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET_KEY } = require('../common/config')
+
 module.exports = (req, res, next) => {
-  req.payload = {
-    id: '1',
+  const authHeader = req.header('Authorization')
+
+  if (authHeader !== undefined) {
+    const tokenString = req.header('Authorization')
+
+    const [type, token] = tokenString.split(' ')
+
+    if (type !== 'Bearer') {
+      res.status(401).send('Wrong auth schema')
+    } else {
+      try {
+        const user = jwt.verify(token, JWT_SECRET_KEY)
+        req.user = user
+      } catch (error) {
+        res.status(401).send('Unauthorized user!')
+      }
+      return next()
+    }
   }
-  return next()
 }
